e5b38a915477b54bb8ce4e121b68c8622418a61f
Min's Git <99301568+dattko@users.noreply.github.com>
2024-09-23 15:19:04 +0900

    폰트 추가 / basurl 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+    // prependData: `@import "function.scss"; @import "base.scss";`,
+  },
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  reactStrictMode: false,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
