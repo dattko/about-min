ebee14471fe7f955757b595b519133cb15fd5869
Min's Git <99301568+dattko@users.noreply.github.com>
2024-09-26 01:50:41 +0900

    메인 페이지 완성

--- src/components/ui/sectionContents/SkillsContent.tsx
+++ src/components/ui/sectionContents/SkillsContent.tsx
@@ -1,19 +1,70 @@
-import React from 'react';
+'use client';
+import React, { useState, useRef, useEffect } from 'react';
 import styles from './Content.module.scss';
 import { skills } from '@/data/contents/skills';
+
 const SkillsContent = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.unobserve(entry.target);
+        }
+      },
+      { threshold: 1 }
+    );
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, []);
+
+  const basicSkills = skills.filter(skill => skill.type === 'basic');
+  const additionalSkills = skills.filter(skill => skill.type === 'additional');
+
   return (
-    <div className={styles.skills__wrap}>
-          {skills.map((skill) => (
+      <div ref={containerRef} className={styles.skills__wrap}>
+        <div className={styles.skills__type}>
+          {basicSkills.map((skill) => (
             <div key={skill.id} className={styles.skills__item}>
+              {skill.percent && (
+                <div className={styles.skills__percent}>
+                  <div
+                    className={`${styles.skills__percentBar} ${isVisible ? styles.animate : ''}`}
+                    style={{ 
+                      width: isVisible ? `${skill.percent}%` : '0%',
+                    }}
+                  ></div>
+                </div>
+              )}
               <div className={styles.skills__img}>
-                {skill.url === '' ? null 
-                 : <img src={skill.url} alt={skill.name} />}
+                {skill.url && <img src={skill.url} alt={skill.name} />}
               </div>
               <span className={styles.skills__title}>{skill.name}</span>
             </div>
           ))}
-    </div>
+        </div>
+        <div className={styles.skills__type}>
+          {additionalSkills.map((skill) => (
+            <div key={skill.id} className={styles.skills__item}>
+              <div className={styles.skills__img}>
+                {skill.url && <img src={skill.url} alt={skill.name} />}
+              </div>
+              <span className={styles.skills__title}>{skill.name}</span>
+            </div>
+          ))}
+        </div>
+      </div>
   );
 }
 
--- src/data/contents/skills.ts
+++ src/data/contents/skills.ts
@@ -2,63 +2,83 @@ export interface Skill {
   id: number;
   name: string;
   url: string;
+  percent?: number;
+  type: 'basic' | 'additional'; // 스킬 유형을 나타내는 필드 추가
 }
 
-export const skills : Skill[] = [
+export const skills: Skill[] = [
   {
-    id : 1,
+    id: 1,
     name: 'HTML',
-    url: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg'
+    url: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg',
+    percent: 90,
+    type: 'basic',
   },
   {
-    id : 2,
+    id: 2,
     name: 'SASS',
-    url: 'https://sass-lang.com/assets/img/logos/logo.svg'
+    url: 'https://sass-lang.com/assets/img/logos/logo.svg',
+    percent: 90,
+    type: 'basic',
   },
   {
-    id : 3,
+    id: 3,
     name: 'TypeScript',
-    url: 'https://upload.wikimedia.org/wikipedia/commons/f/f5/Typescript.svg'
+    url: 'https://upload.wikimedia.org/wikipedia/commons/f/f5/Typescript.svg',
+    percent: 70,
+    type: 'basic',
   },
   {
-    id : 4,
+    id: 4,
     name: 'JavaScript',
-    url: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg'
+    url: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg',
+    percent: 60,
+    type: 'basic',
   },
   {
-    id : 5,
+    id: 5,
     name: 'Vue.js',
-    url: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Vue.png'
+    url: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Vue.png',
+    percent: 50,
+    type: 'basic',
   },
   {
-    id : 6,
+    id: 6,
     name: 'React.js',
-    url: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
+    url: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+    percent: 70,
+    type: 'basic',
   },
   {
-    id : 7,
+    id: 7,
     name: 'React Native',
-    url: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
+    url: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+    percent: 40,
+    type: 'basic',
   },
   {
-    id : 8,
+    id: 8,
     name: 'Angular.js',
-    url: 'https://www.angular.kr/assets/images/logos/angular/angular.svg'
+    url: 'https://www.angular.kr/assets/images/logos/angular/angular.svg',
+    percent: 60,
+    type: 'basic',
   },
   {
-    id : 9,
+    id: 9,
     name: 'Vercel',
-    url: 'https://camo.githubusercontent.com/e40cb80a2b7078aaa29e48190c4d665cdfb0a56a0dc871638c7f81efbe230fdb/68747470733a2f2f6173736574732e76657263656c2e636f6d2f696d6167652f75706c6f61642f76313538383830353835382f7265706f7369746f726965732f76657263656c2f6c6f676f2e706e67'
+    url: 'https://camo.githubusercontent.com/e40cb80a2b7078aaa29e48190c4d665cdfb0a56a0dc871638c7f81efbe230fdb/68747470733a2f2f6173736574732e76657263656c2e636f6d2f696d6167652f75706c6f61642f76313538383830353835382f7265706f7369746f726965732f76657263656c2f6c6f676f2e706e67',
+    type: 'additional',
   },
   {
-    id : 10,
+    id: 10,
     name: 'GitHub',
-    url: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Font_Awesome_5_brands_github.svg'
+    url: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Font_Awesome_5_brands_github.svg',
+    type: 'additional',
   },
   {
-    id : 11,
+    id: 11,
     name: 'GitLab',
-    url: 'https://images.ctfassets.net/xz1dnu24egyd/3FbNmZRES38q2Sk2EcoT7a/a290dc207a67cf779fc7c2456b177e9f/press-kit-icon.svg'
-  }
-
+    url: 'https://images.ctfassets.net/xz1dnu24egyd/3FbNmZRES38q2Sk2EcoT7a/a290dc207a67cf779fc7c2456b177e9f/press-kit-icon.svg',
+    type: 'additional',
+  },
 ];
