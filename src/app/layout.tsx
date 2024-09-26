3956238467cdb1f28a18755e1facc78e382f28a9
Min's Git <99301568+dattko@users.noreply.github.com>
2024-09-27 00:09:54 +0900

    파비콘 변경 디스크립션 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,11 @@ import ContactMe from '@/components/button/ContactMe';
 import Footer from '@component/layouts/footer/Footer';
 import { ScrollProvider } from '@/contexts/ScrollContext';
 export const metadata: Metadata = {
-  title: "about-min"
+  title: "about-min",
+  description: "저의 포트폴리오 사이트입니다.",
+  icons: {
+		icon: "/favicon.png",
+	},
 };
 
 export default function RootLayout({
@@ -23,6 +27,6 @@ export default function RootLayout({
           <Footer/>
         </ScrollProvider>
       </body>
-    </html>
+  </html>
   );
 }
