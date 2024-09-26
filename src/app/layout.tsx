0ef197af089e61f81931edc14b6ce3d57e7ff056
Min's Git <99301568+dattko@users.noreply.github.com>
2024-09-27 00:17:57 +0900

    og이미지 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,23 @@ import ContactMe from '@/components/button/ContactMe';
 import Footer from '@component/layouts/footer/Footer';
 import { ScrollProvider } from '@/contexts/ScrollContext';
 export const metadata: Metadata = {
-  title: "about-min",
+  title: "AboutMin",
   description: "저의 포트폴리오 사이트입니다.",
   icons: {
 		icon: "/favicon.png",
 	},
+  openGraph: {
+    title: "AboutMin",
+    description: "저의 포트폴리오 사이트입니다.",
+    url: "https://aboutmin.info", 
+    images: [
+      {
+        url: "/og.png", 
+      },
+    ],
+    siteName: "AboutMin",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
